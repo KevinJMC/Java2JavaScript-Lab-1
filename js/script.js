c5684cff2c9c81264389b12d4f26d3886981633f
Kevin McCann <kevinmccann@zipcoders-MacBook-Pro-5.local>
2017-01-19 19:00:10 -0500

    1-7 done, need 9/10

--- js/script.js
+++ js/script.js
@@ -1,7 +1,87 @@
-" use strict ";
+'use strict';
 
 var display = document.getElementById("display");
 
-function yourFunctionName (){
+function yourFunctionName() {
   display.innerHTML = "hello";
+}
+
+function oneToTen() {
+    display.innerHTML = "";
+    var i;
+    for (i = 1; i <= 10; i++) {
+        display.innerHTML += i + "<br />";
+    }
+}
+
+function oddNumbers() {
+    display.innerHTML = "";
+    var i;
+    for (i = 1; i < 20; i = i + 2) {
+        display.innerHTML += i + "<br />";
+    }
+}
+
+function squares() {
+    display.innerHTML = "";
+    var i;
+    for (i = 1; i <= 10; i++) {
+        display.innerHTML += (i * i) + "<br />";
+    }
+}
+
+function random4() {
+    display.innerHTML = "";
+    var i;
+    for (i = 1; i <= 4; i++) {
+        display.innerHTML += Math.floor(Math.random() * 100) + "<br />";
+    }
+}
+
+function even(n) {
+    display.innerHTML = "";
+    var i;
+    for (i = 2; i < n; i = i + 2) {
+        display.innerHTML += i + "<br />";
+    }
+}
+
+function powers(n) {
+    display.innerHTML = "";
+    var i;
+    for (i = 2; i <= n; i++) {
+        display.innerHTML += Math.pow(2, i) + "<br />";
+    }
+}
+
+//function areWeThereYet() {
+//    display.innerHTML = "";
+//    var i, input;
+//    do {
+//        display.innerHTML += "Are we there yet?" + "<br />" + "<input id=\"input\"/>";
+//        input = document.getElementById("input").value;
+//    } while (input.toLowerCase() !== "yes");
+//    display.innerHTML += "Good!";
+//}
+
+function triangle(n) {
+    display.innerHTML = "";
+    var i, j;
+    for (i = 1; i <= n; i++) {
+        for (j = 1; j <= i; j++) {
+            display.innerHTML += "*";
+        }
+        display.innerHTML += "<br />";
+    }
+}
+
+function tableSquare(n) {
+    display.innerHTML = "";
+    var i, j;
+    for (i = 1; i <= n; i++) {
+        for (j = i; j <= n*n; j++) {
+            display.innerHTML += j + " ";
+        }
+        display.innerHTML += "<br />";
+    }
 }
